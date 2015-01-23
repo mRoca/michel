054b26300875e89ad37aa1fe4ae8e99e9992b7b2
mRoca <mroca.dh@gmail.com>
2015-01-23 11:02:35 +0100

    Added announcement list filters
    Ref #10

--- src/SensioLabs/JobBoardBundle/Resources/assets/common.js
+++ src/SensioLabs/JobBoardBundle/Resources/assets/common.js
@@ -1,34 +1,21 @@
-$(document).ready(function(){
+$(function () {
 
-	$('.filter-action').click(function(){
-		var nextMove = $(this).children('img').attr('alt');
-		var contentSeeMore = '<img class="sprite-icon-dark-arrow-down" src="';
-		contentSeeMore = contentSeeMore.concat(blank);
-		contentSeeMore = contentSeeMore.concat('" alt="v" /> ');
+    $('.filter-action').on('click', function (e) {
 
-		var contentSeeLess = '<img class="sprite-icon-dark-arrow-up" src="';
-		contentSeeLess = contentSeeLess.concat(blank);
-		contentSeeLess = contentSeeLess.concat('" alt="^" /> ');
+        e.preventDefault();
 
-		if (nextMove == '^')
-		{
-			$(this).parent().parent().children().each(function(index) {
-				if (index > 3 && $(this).children('a').attr('class') != 'filter-action' && $(this).attr('class') != 'active')
-					$(this).hide(300);
-				else if ($(this).children('a').attr('class') == 'filter-action')
-					$(this).children('a').html(contentSeeMore.concat(seeMore));
-			});
-		}
-		else if (nextMove == 'v')
-		{
-			$(this).parent().parent().children().each(function(index) {
-				if (index > 3 && $(this).children('a').attr('class') != 'filter-action' && $(this).attr('class') != 'active')
-					$(this).show(300);
-				else if ($(this).children('a').attr('class') == 'filter-action')
-					$(this).children('a').html(contentSeeLess.concat(seeLess));
-			});
-		}
-		return false;
-	});
+        var $this = $(this);
+        var currentAction = $this.attr('data-filter-action');
+        var nextAction = currentAction == 'up' ? 'down' : 'up';
+        var $container = $this.closest('ul');
 
+        $container.find('li:gt(3):not(:last)').each(function () {
+            nextAction == 'down' ? $(this).hide(300) : $(this).show(300);
+        });
+
+        $container.find('[data-filter-action=' + currentAction + ']').hide();
+        $container.find('[data-filter-action=' + nextAction + ']').show();
+    });
+
+    $('.filter li.active:not(:visible)').parent().find('a[data-filter-action=down]').click();
 });
